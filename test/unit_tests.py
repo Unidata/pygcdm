c5a0d16a2ceedebcc9974e9ca318ac5b0427e70a
Rio McMahon <rmcmahon@ucar.edu>
2021-09-17 11:43:24 -0600

    tryna get coverage up

--- test/unit_tests.py
+++ test/unit_tests.py
@@ -1,7 +1,7 @@
 import pytest
 import importlib
 pygcdm = importlib.import_module("pygcdm", package="pygcdm")
-msg = pygcdm.protogen.gcdm_netcdf_pb2
+grpc_msg = pygcdm.protogen.gcdm_netcdf_pb2
 import netCDF4 as nc4
 import os
 
@@ -15,7 +15,7 @@ def test_interpret_spec():
     section = encoder._interpret_slices(slices, [*nc.dimensions.values()])
     sizes = [1, 720, 1440]
     strides = [1, 1, 1]
-    assert section == msg.Section(ranges=[msg.Range(size=s, stride=st) for s,st in zip(sizes, strides)])
+    assert section == grpc_msg.Section(ranges=[grpc_msg.Range(size=s, stride=st) for s,st in zip(sizes, strides)])
 
     # test variable name, ellipses
     var_spec = 'analysed_sst(:,:,:)'
@@ -23,7 +23,7 @@ def test_interpret_spec():
     section = encoder._interpret_slices(slices, [*nc.dimensions.values()])
     sizes = [1, 720, 1440]
     strides = [1, 1, 1]
-    assert section == msg.Section(ranges=[msg.Range(size=s, stride=st) for s,st in zip(sizes, strides)])
+    assert section == grpc_msg.Section(ranges=[grpc_msg.Range(size=s, stride=st) for s,st in zip(sizes, strides)])
 
     # test variable name, ellipses w/ space
     var_spec = 'analysed_sst(:, :, :)'
@@ -31,7 +31,7 @@ def test_interpret_spec():
     section = encoder._interpret_slices(slices, [*nc.dimensions.values()])
     sizes = [1, 720, 1440]
     strides = [1, 1, 1]
-    assert section == msg.Section(ranges=[msg.Range(size=s, stride=st) for s,st in zip(sizes, strides)])
+    assert section == grpc_msg.Section(ranges=[grpc_msg.Range(size=s, stride=st) for s,st in zip(sizes, strides)])
 
     # test variable name, slices
     var_spec = 'analysed_sst(0, 0:719:10, 0:1439:10)'
@@ -39,7 +39,7 @@ def test_interpret_spec():
     section = encoder._interpret_slices(slices, [*nc.dimensions.values()])
     sizes = [1, 72, 144]
     strides = [1, 10, 10]
-    assert section == msg.Section(ranges=[msg.Range(size=s, stride=st) for s,st in zip(sizes, strides)])
+    assert section == grpc_msg.Section(ranges=[grpc_msg.Range(size=s, stride=st) for s,st in zip(sizes, strides)])
 
     # test variable name, slices, starts
     var_spec = 'analysed_sst(0, 200:700:10, 1300:1400:10)'
@@ -48,4 +48,4 @@ def test_interpret_spec():
     sizes = [1, 51, 11]
     strides = [1, 10, 10]
     starts = [None, 200, 1300]
-    assert section == msg.Section(ranges=[msg.Range(size=s, stride=st, start=sa) for s,st,sa in zip(sizes, strides, starts)])
+    assert section == grpc_msg.Section(ranges=[grpc_msg.Range(size=s, stride=st, start=sa) for s,st,sa in zip(sizes, strides, starts)])
